a3eb47e43506e145269ff7eef3510c0ec4f93a91
Daniel Krebser <daniel.krebser@boxler.com>
2022-05-09 21:22:14 +0200

    Initial Solution Structure

---
+++ CSM.UiLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSM.UiLogic
+{
+    public class Class1
+    {
+    }
+}
