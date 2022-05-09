9cc857fabf1b0b7fadff42d5dd48af92238f5adc
Daniel Krebser <daniel.krebser@boxler.com>
2022-05-09 21:32:59 +0200

    Folder Structure

--- CSM.App/Properties/AssemblyInfo.cs
+++ CSM.App/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSM.App")]
+[assembly: AssemblyTitle("Custom Songs Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSM.App")]
+[assembly: AssemblyProduct("Custom Songs Manager")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
