6362a2246ed20e305518f9a59541097eec912192
Daniel Krebser <daniel@boxler.com>
2025-04-17 15:57:16 +0200

    show playlist song info

--- CSM.UiLogic/ViewModels/Common/Playlists/PlaylistSongViewModel.cs
+++ CSM.UiLogic/ViewModels/Common/Playlists/PlaylistSongViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CSM.DataAccess.Playlists;
 using CSM.Framework.ServiceLocation;
 using CSM.UiLogic.AbstractBase;
+using System.Globalization;
 
 namespace CSM.UiLogic.ViewModels.Common.Playlists
 {
@@ -8,6 +9,30 @@ namespace CSM.UiLogic.ViewModels.Common.Playlists
         : BaseViewModel(serviceLocator)
     {
         private Song song = song;
+
+        #region Properties
+
+        public string Hash => song.Hash.ToLower();
+
+        public string BsrKey => song.Key ?? string.Empty;
+
+        public int BsrKeyHey
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(song.Key))
+                    return 0;
+                int.TryParse(BsrKey, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out int result);
+                return result;
+            }
+        }
+
+        public string SongName => song.SongName;
+
+        public string? LevelAuthorName => song.LevelAuthorName;
+
+        #endregion
+
     }
 
 }
