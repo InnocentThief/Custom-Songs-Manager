d5e0f822844740454c32b726a938d2594a12c99c
InnocentThief <github@krebser.info>
2025-04-24 15:36:00 +0200

    map detail

--- CSM.UiLogic/ViewModels/Common/MapDetails/MapDifficultyViewModel.cs
+++ CSM.UiLogic/ViewModels/Common/MapDetails/MapDifficultyViewModel.cs
@@ -27,9 +27,11 @@ namespace CSM.UiLogic.ViewModels.Common.MapDetails
         {
             get
             {
-                var infos = new List<string>();
-                infos.Add($"NPS: {Math.Round(difficulty.Nps, 2)}");
-                infos.Add($"NJS: {Math.Round(difficulty.Njs, 2)}");
+                var infos = new List<string>
+                {
+                    $"NPS: {Math.Round(difficulty.Nps, 2)}",
+                    $"NJS: {Math.Round(difficulty.Njs, 2)}"
+                };
                 if (difficulty.Chroma)
                     infos.Add("CR");
                 if (difficulty.Me)
