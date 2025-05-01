9f639b3857d6c146f3c9266960ee5416c0bce461
InnocentThief <github@krebser.info>
2025-05-01 12:01:20 +0200

    stuff

--- CSM.UiLogic/ViewModels/Workspaces/TwitchWorkspaceViewModel.cs
+++ CSM.UiLogic/ViewModels/Workspaces/TwitchWorkspaceViewModel.cs
@@ -1,5 +1,7 @@
-﻿using CSM.Framework.ServiceLocation;
+﻿using CSM.Business.Core.SongSelection;
+using CSM.Framework.ServiceLocation;
 using CSM.UiLogic.AbstractBase;
+using CSM.UiLogic.ViewModels.Controls.PlaylistsTree;
 
 namespace CSM.UiLogic.ViewModels.Workspaces
 {
@@ -7,13 +9,18 @@ namespace CSM.UiLogic.ViewModels.Workspaces
     {
         public override string Title => "Custom Songs Manager - Twitch";
 
+        public PlaylistTreeControlViewModel PlaylistsTree { get; }
+
         public TwitchWorkspaceViewModel(IServiceLocator serviceLocator) : base(serviceLocator)
         {
+            PlaylistsTree = new PlaylistTreeControlViewModel(ServiceLocator, SongSelectionType.Left, true);
         }
 
         public override async Task ActivateAsync(bool refresh)
         {
-            await Task.CompletedTask;
+            var playlistTreeTask = PlaylistsTree.LoadAsync(refresh);
+
+            await Task.WhenAll(playlistTreeTask);
         }
     }
 }
