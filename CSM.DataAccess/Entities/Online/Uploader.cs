68f882097f9ffed45e973fbe606418b1f3fcdad2
InnocentThief <github@krebser.info>
2025-05-25 13:35:01 +0200

    Remove version 1

--- CSM.DataAccess/Entities/Online/Uploader.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CSM.DataAccess.Entities.Online
-{
-    /// <summary>
-    /// Represents the BeatMap uploader (as used in BeatSaver).
-    /// </summary>
-    public class Uploader
-    {
-    }
-}
--- CSM.UiLogic/Workspaces/TwitchIntegration/ScoreSaberAddPlayerEventArgs.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CSM.UiLogic.Workspaces.TwitchIntegration
-{
-    public class ScoreSaberAddPlayerEventArgs
-    {
-        public string Playername { get; set; }
-    }
-}
